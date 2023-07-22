71c3b54b3df3a9a8f53c78ea94c5c9df1a73e0c4
BerkayDemirciOfficial <138025229+BerkayDemirciOfficial@users.noreply.github.com>
2023-07-22 21:12:28 +0300

    Base Calculation With Recursive
    Base Calculation With Recursive

---
+++ Java-101/src/BaseCalculationWithRecursive.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class BaseCalculationWithRecursive {
+
+
+    static int baseCalculation(int base, int power) {
+        if (power == 0) {
+            return 1;
+        }
+        return base * baseCalculation(base, power - 1);
+
+    }
+
+    public static void main(String[] args) {
+
+        int base, power, result;
+
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Base : ");
+        base = inp.nextInt();
+
+        System.out.print("Power : ");
+        power = inp.nextInt();
+
+        result = baseCalculation(base, power);
+
+        System.out.print("Result : " + result);
+
+
+    }
+}
