46d36cbee1788ba0c9c6863e77a5a1beb3368211
BerkayDemirciOfficial <138025229+BerkayDemirciOfficial@users.noreply.github.com>
2023-09-07 16:50:55 +0300

    Finding Smaller Number And Bigger Number
    Finding Smaller Number And Bigger Number

---
+++ Java-101/src/FindingSmallerNumberAndBiggerNumber.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class FindingSmallerNumberAndBiggerNumber {
+
+    public static void main(String[] args) {
+
+        int[] list = {-3, 7, 18, 23, 42, -15};
+
+        int nearMin = Integer.MIN_VALUE;
+        int nearMax = Integer.MAX_VALUE;
+        int user;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a Number : ");
+        user = input.nextInt();
+
+        for (int i : list) {
+
+            if (i< user) {
+
+                if (i > nearMin) {
+
+                    nearMin = i;
+                }
+            }
+            if (i > user) {
+
+                if (i< nearMax) {
+
+                    nearMax = i;
+                }
+            }
+        }
+        System.out.println("Near Mİn Number : " + nearMin);
+        System.out.println("Near Max Number : " + nearMax);
+
+    }
+}
+
