ae4464448320769b55cabbaa2abe70faf1547be0
BerkayDemirciOfficial <138025229+BerkayDemirciOfficial@users.noreply.github.com>
2023-09-11 19:48:31 +0300

    Update FindingPalindromeWords.java

--- Java-101/src/FindingPalindromeWords.java
+++ Java-101/src/FindingPalindromeWords.java
@@ -1,22 +1,11 @@
 public class FindingPalindromeWords {
 
-    static boolean isWord(String str) {
-
-        int i = 0;
-        int j = str.length() - 1;
-
-        if (str.charAt(i) == str.charAt(j)) {
-            return true;
-        }
-        return false;
-    }
 
     public static void main(String[] args) {
 
-        System.out.print(isWord("edippide"));
 
     }
-
-
 }
+
+
 
